ef6e041c1f4523b2cb74338f9415babe92584dd2
Your Name <you@example.com>
2025-02-24 01:15:57 +0500

    Добавьте файлы проекта.

---
+++ home.cs
@@ -0,0 +1,19 @@
+﻿using lab;
+class Progam
+{
+    static void Main(string[] args)
+    {
+        char choice;
+        Console.WriteLine("Operations between 2 num");
+        Console.WriteLine("know number exponent");
+        Console.WriteLine("know number root");
+        choice = Console.ReadKey().KeyChar;
+
+        switch (choice) {
+        
+        
+        
+        }
+
+    }
+}
---
+++ lab.cs
@@ -0,0 +1,25 @@
+﻿namespace lab
+{
+    class Calc
+    {
+        public static void Solve(double a,double b,char op)
+        {
+           switch (op)
+            {
+                case '+':
+                    Console.WriteLine(a + b); break;
+                case '-':
+                    Console.WriteLine(a - b); break;
+                case '/':
+                    if (b == 0){
+                        Console.WriteLine("are u know that you cant do this or u just playing with me?");
+                    }
+                    break;
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+                        
+            }
+        }
+    }
+}
